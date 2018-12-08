4671387405043583c6c8617b5a2b7d297c38d3dd
Peter Snoek <petersnoek@davinci.nl>
2018-12-08 22:02:55 +0100

    Created starting page and login/register

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/logout', 'Auth\LoginController@logout');       // fix: logout should be a POST as of Laravel 5.3
+
 Auth::routes();
 
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+
